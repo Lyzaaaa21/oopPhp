e64d37df2fc9a60b46e99edda3c006f8d8f7a7f6
Carmel Lyza <148184575+Lyzaaaa21@users.noreply.github.com>
2024-03-06 12:33:31 +0800

    Add files via upload

---
+++ oopPhp/vid3/newclass.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+class NewClass {
+
+    //properties and methods goes here
+    public $info = "Magrereview pa ako mamaya";
+}
+
+$object = new NewClass;
+var_dump($object);
---
+++ oopPhp/vid4/person.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+class Person {
+    private $first = "Lyza";
+    private $last = "Lauzon";
+    private $age = "19";
+    private $occupation = "Engineering Student";
+
+    public function owner() {
+
+        $a = $this->first;
+        return $a;
+    }
+}
+class Pet {
+
+    // public function owner() {
+    //     $a = "Hi there!";
+    //     return $a; 
+        
+    // }
+}
+
+
